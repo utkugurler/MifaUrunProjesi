7cf2c7470808adc420f8fcd59304ae8879c16e26
utku gurler <utku.gurler@hotmail.com>
2020-10-25 17:38:09 +0300

    Ana sayfa başlangıcı değiştirildi.

--- MifaUretim/Startup.cs
+++ MifaUretim/Startup.cs
@@ -54,7 +54,7 @@ namespace MifaUretim
 			{
 				endpoints.MapControllerRoute(
 					name: "default",
-					pattern: "{controller=Home}/{action=Index}/{id?}");
+					pattern: "{controller=Urunler}/{action=Index}/{id?}");
 			});
 		}
 	}
